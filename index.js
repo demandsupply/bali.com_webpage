4884aa487d9162d26760226cff017ab48940bad0
demandsupply <giova.zol92@gmail.com>
2023-08-31 20:14:02 +0200

    created js function which combines the two previous functions (functions A & B)

--- index.js
+++ index.js
@@ -1,43 +1,71 @@
-// Function ONE:
-//  Create a function which shows the clicked div and hide all the others
+// Function ONE (function A + function B)
+// Create a function which:
+// 1 - shows / hide the div when the user clicks on it
+// 2 - shows the clicked div and hide all the others
 
 function showAnswer(id) {
     if (document.getElementById) {
         var answerId = document.getElementById(id);
         var allAnswers = document.getElementsByClassName("faq-answer");
         for (var i=0; i < allAnswers.length; i++) {
-            allAnswers[i].style.display = "none";
+            if (allAnswers[i] != answerId) {
+                allAnswers[i].style.display = "none";
+            } else {
+                if (answerId.style.display == "none" || answerId.style.display == "") {
+                    answerId.style.display = "block";
+                } else {
+                    answerId.style.display = "none";
+                }
+            }
         }
-        answerId.style.display = "block";
     }
     return false;
 }
 
-function toggleAnswer1() {
-    var answer = document.getElementById("faq-1")
-    var displaySetting = answer.style.display;
+// Function A:
+// Create a function which show / hide the div when the user clicks on it
 
-    if (displaySetting == 'block') {
-        answer.style.display = 'none';
-    }
-    else {
-        answer.style.display = 'block';
-    }
-}
+// function toggleAnswer1() {
+//     var answer = document.getElementById("faq-1")
+//     var displaySetting = answer.style.display;
 
-function toggleAnswer2() {
-    var answer = document.getElementById("faq-2")
-    var displaySetting = answer.style.display;
-
-    if (displaySetting == 'block') {
-        answer.style.display = 'none';
-    }
-    else {
-        answer.style.display = 'block';
-    }
-}
-
-// function all() {
-//     showAnswer(id);
-//     toggleAnswer(id);
+//     if (displaySetting == 'block') {
+//         answer.style.display = 'none';
+//     }
+//     else {
+//         answer.style.display = 'block';
+//     }
 // }
+
+// function toggleAnswer2() {
+//     var answer = document.getElementById("faq-2")
+//     var displaySetting = answer.style.display;
+
+//     if (displaySetting == 'block') {
+//         answer.style.display = 'none';
+//     }
+//     else {
+//         answer.style.display = 'block';
+//     }
+// }
+
+
+// Function B:
+//  Create a function which shows the clicked div and hide all the others
+
+// function showAnswer(id) {
+//     if (document.getElementById) {
+//         var answerId = document.getElementById(id);
+//         var allAnswers = document.getElementsByClassName("faq-answer");
+//         for (var i=0; i < allAnswers.length; i++) {
+//             allAnswers[i].style.display = "none";
+//         }
+//         answerId.style.display = "block";
+//     }
+//     return false;
+// }
+
+
+
+
+
